eacdfe9cc8099d7d9be2c82d184ef59df1383ee9
yvonnebongers <yvonnebongers@gmail.com>
2025-12-28 22:00:40 +0100

    opdracht 1 gemaakt

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import './App.css'
-import logo from './assets/logo-white.png'
+import './App.css';
+import logo from './assets/logo-white.png';
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/home/Home';
+import BlogPosts from './pages/blogPosts/BlogPosts';
+import OverviewPage from './pages/overviewPage/OverviewPage';
+import NotFoundPage from './pages/notFoundPage/NotFoundPage';
+
 
 function App() {
     return (
-        <div className="page-container">
+        <>
+            <Routes>
+               <Route path="/" element={<Home/>}/>
+                <Route path="/blogposts" element={<BlogPosts/>}/>
+                <Route path="/overviewpage" element={<OverviewPage/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Routes>
+
+            <div className="page-container">
             <img src={logo} alt="Company logo"/>
             <h1>Begin hier met het maken van jouw blog-applicatie!</h1>
         </div>
+        </>
     )
 }
 
